5b666378a64a1bd67da0a50f1caa5a4a76f1f7d1
Karan Kumar <kumarkay.ca@gmail.com>
2019-12-05 15:28:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/group10_f2019_mad3125_fp/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.group10_f2019_mad3125_fp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+        Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+
+
+
+    }
+}
