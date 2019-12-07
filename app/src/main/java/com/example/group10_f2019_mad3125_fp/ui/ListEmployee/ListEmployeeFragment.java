34e8fdfff53f8c6943e5047b423b86dd0ab685e1
Karan <kumarkay.ca@gmail.com>
2019-12-06 20:21:28 -0500

    employee

--- app/src/main/java/com/example/group10_f2019_mad3125_fp/ui/ListEmployee/ListEmployeeFragment.java
+++ app/src/main/java/com/example/group10_f2019_mad3125_fp/ui/ListEmployee/ListEmployeeFragment.java
@@ -11,25 +11,46 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.group10_f2019_mad3125_fp.R;
+import com.example.group10_f2019_mad3125_fp.RecyclerViewAdapter;
+import com.example.group10_f2019_mad3125_fp.Singleton;
+import com.example.group10_f2019_mad3125_fp.model.employee.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListEmployeeFragment extends Fragment {
 
-    private ListEmployeeViewModel listEmployeeViewModel;
+    private RecyclerView rvEmployeeList;
+    private List<Employee> employeeList;
+    Singleton singleton=Singleton.getInstance();
+    RecyclerViewAdapter recyclerViewAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        listEmployeeViewModel =
-                ViewModelProviders.of(this).get(ListEmployeeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_list_emloyee, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        listEmployeeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        rvEmployeeList=root.findViewById(R.id.empListRV);
         return root;
     }
+
+
+    private void initViews() {
+        populateData();
+
+        recyclerViewAdapter = new RecyclerViewAdapter(employeeList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        rvEmployeeList.setLayoutManager(layoutManager);
+        rvEmployeeList.setAdapter(recyclerViewAdapter);
+    }
+    private void populateData()
+    {
+        employeeList = new ArrayList<>();
+        employeeList = singleton.getEmployees();
+
+
+    }
+
 }
