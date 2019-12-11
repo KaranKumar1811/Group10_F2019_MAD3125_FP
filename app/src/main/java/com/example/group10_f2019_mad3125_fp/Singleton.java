8ac460035a034e661162b7acfd21fe5c649cb172
Karan Kumar <kumarkay.ca@gmail.com>
2019-12-10 20:01:43 -0500

    init

--- app/src/main/java/com/example/group10_f2019_mad3125_fp/Singleton.java
+++ app/src/main/java/com/example/group10_f2019_mad3125_fp/Singleton.java
@@ -3,8 +3,12 @@ package com.example.group10_f2019_mad3125_fp;
 
 
 import com.example.group10_f2019_mad3125_fp.model.employee.Employee;
+import com.example.group10_f2019_mad3125_fp.model.employee.employeeType.FullTime;
 import com.example.group10_f2019_mad3125_fp.model.employee.employeeType.Intern;
+import com.example.group10_f2019_mad3125_fp.model.employee.employeeType.partTime.CommissionBasedPartTime;
+import com.example.group10_f2019_mad3125_fp.model.employee.employeeType.partTime.FixedBasedPartTime;
 import com.example.group10_f2019_mad3125_fp.model.vehicle.Car;
+import com.example.group10_f2019_mad3125_fp.model.vehicle.Motorcycle;
 
 import java.util.ArrayList;
 
@@ -21,6 +25,21 @@ public class Singleton {
         Car c1=new Car("Swift","ABC123","Red",2018,20,4);
         i1.setVehicle(c1);
         addEmployee(i1);
+
+        FullTime f1=new FullTime("Karan",23,3400,300);
+        Car c2=new Car("Lamborghini","Kay1811","Yellow",2019,10,2);
+        f1.setVehicle(c2);
+        addEmployee(f1);
+
+        CommissionBasedPartTime cmPt1=new CommissionBasedPartTime("Kamal",26,14,20,5);
+        Motorcycle m1=new Motorcycle("Hero","Kamal123","Red",2004,2,50);
+        cmPt1.setVehicle(m1);
+        addEmployee(cmPt1);
+
+        FixedBasedPartTime fbPt=new FixedBasedPartTime("Sandy",22,20,100,100);
+        Motorcycle m2=new Motorcycle("Yamaha","Sandy123","Black",2019,1000,400);
+        fbPt.setVehicle(m2);
+        addEmployee(fbPt);
     }
 
     /**
